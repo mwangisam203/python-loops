e8a0faaf857e36f6a35d6e2b004953c2f10995c6
mwangisam203 <mwangisam203@gmail.com>
2025-12-30 02:28:33 -0500

    for loops

---
+++ loops.py
@@ -0,0 +1,7 @@
+mytuple = ("Nairobi", "Cairo", "Lagos", "Rabat")
+for x in mytuple:
+  print(x)  #output each item in the tuple :
+            #Nairobi
+            #Cairo
+            #Lagos
+            #Rabat
