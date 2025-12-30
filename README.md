41ddb890d24642782212b30cffae07dfc20c2e9f
mwangisam203 <mwangisam203@gmail.com>
2025-12-30 03:46:16 -0500

    mdified readme and loop.py

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 *** In this project, I will mainly focus on python loops ***
-***
+***Looping using range() (Index-Based Loop) ***
+    You loop over indexes
+    You must manually access values using fruits[i]
+    
--- loops.py
+++ loops.py
@@ -1,3 +1,4 @@
+#Loop through a tuple
 mytuple = ("Nairobi", "Cairo", "Lagos", "Rabat")
 for x in mytuple:
   print(x)  #output each item in the tuple :
@@ -5,3 +6,19 @@ for x in mytuple:
             #Cairo
             #Lagos
             #Rabat
+
+#Loop through the index numbers
+for i in range(len(mytuple)):
+  print(mytuple[i]) #output each item in the tuple :
+                    #Nairobi
+                    #Cairo
+                    #Lagos
+                    #Rabat            
+
+#Loop through both index and value
+for i in enumerate(mytuple):
+  print(i)  #output each item in the tuple with its index :
+            #(0, 'Nairobi')
+            #(1, 'Cairo')
+            #(2, 'Lagos')
+            #(3, 'Rabat')
